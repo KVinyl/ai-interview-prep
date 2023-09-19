db7e2541e4eb1bdb135a6805968a7fd4e159d94c
Kevin Lie <kevinyilie@gmail.com>
2023-09-19 16:16:04 -0400

    Initialize InterviewPrep

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
     <AppHeader />
+    <InterviewPrep />
   </div>
 </template>
 
 <script setup lang="ts">
 import AppHeader from './components/AppHeader.vue';
-
+import InterviewPrep from './components/InterviewPrep.vue';
 </script>
 
 <style scoped></style>
---
+++ src/components/InterviewPrep.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-if="question">
+    <p>{{ question }}</p>
+    <textarea v-model="answer"></textarea>
+    <button @click="getFeedback(answer)">Submit</button>
+    <p>{{ feedback }}</p>
+  </div>
+
+  <div v-else>
+    <h1>End of study session</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+
+const questions: string[] = ["What is git? Why do we use it?",
+  "What is a variable? Can you give an example of an integer expression? A boolean expression?",
+  "What controls whether the “if” or the “else” portion of an if-else should execute?"
+]
+const currentIndex = ref(0)
+const question = computed(() => questions[currentIndex.value])
+const answer = ref("")
+const feedback = ref("")
+
+function getFeedback(prompt: string) {
+    feedback.value = prompt ? `This is the feedback to the answer: ${prompt}`: "Please enter an answer to the question."
+}
+</script>
+
+<style scoped></style>
