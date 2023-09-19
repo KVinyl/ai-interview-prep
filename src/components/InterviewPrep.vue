b641f7037632220f511a8315fc377db18e9dd3e3
Kevin Lie <kevinyilie@gmail.com>
2023-09-19 17:07:29 -0400

    Add reset button for the end of session and display message when no questions exist

--- src/components/InterviewPrep.vue
+++ src/components/InterviewPrep.vue
@@ -4,16 +4,21 @@
     <textarea v-model="answer"></textarea>
     <button @click="getFeedback(answer)">Submit</button>
     <p>{{ feedback }}</p>
-    <button @click="nextQuestion">Next Question</button>
+    <button @click="currentIndex++">Next Question</button>
+  </div>
+
+  <div v-else-if="questions.length">
+    <h2>End of study session</h2>
+    <button @click="currentIndex = 0">Start Over</button>
   </div>
 
   <div v-else>
-    <h1>End of study session</h1>
+    <h2>No questions found</h2>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { ref, computed, watch } from 'vue'
 
 const questions: string[] = ["What is git? Why do we use it?",
   "What is a variable? Can you give an example of an integer expression? A boolean expression?",
@@ -21,18 +26,18 @@ const questions: string[] = ["What is git? Why do we use it?",
 ]
 const currentIndex = ref(0)
 const question = computed(() => questions[currentIndex.value])
+
 const answer = ref("")
 const feedback = ref("")
 
 function getFeedback(prompt: string) {
-    feedback.value = prompt ? `This is the feedback to the answer: ${prompt}`: "Please enter an answer to the question."
+  feedback.value = prompt ? `This is the feedback to the answer: ${prompt}` : "Please enter an answer to the question."
 }
 
-function nextQuestion() {
-  currentIndex.value++
+watch(currentIndex, () => {
   answer.value = ""
   feedback.value = ""
-}
+})
 </script>
 
 <style scoped></style>
