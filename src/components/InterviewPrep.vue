9b5a58bd4163b3c139438ad30abf902c9866e04a
Kevin Lie <kevinyilie@gmail.com>
2023-09-20 19:29:50 -0400

    Add OpenAIService

--- src/components/InterviewPrep.vue
+++ src/components/InterviewPrep.vue
@@ -2,7 +2,7 @@
   <div v-if="question">
     <p>{{ question }}</p>
     <textarea v-model="answer"></textarea>
-    <button @click="getFeedback(answer)">Submit</button>
+    <button @click="getFeedback(question, answer)">Submit</button>
     <p>{{ feedback }}</p>
     <button @click="currentIndex++">Next Question</button>
   </div>
@@ -19,6 +19,7 @@
 
 <script setup lang="ts">
 import { ref, computed, watch } from 'vue'
+import openAIService from '../services/OpenAIService'
 
 const { questions } = defineProps<{
   questions: string[]
@@ -30,8 +31,13 @@ const question = computed(() => questions[currentIndex.value])
 const answer = ref("")
 const feedback = ref("")
 
-function getFeedback(prompt: string) {
-  feedback.value = prompt ? `This is the feedback to the answer: ${prompt}` : "Please enter an answer to the question."
+function getFeedback(question: string, answer: string) {
+  if (question.trim() && answer.trim()) {
+    const prompt = `Suppose I'm seeking a role as a junior software developer. I'm being asked this question in an interview: ${question}\n\nThis is my answer: ${answer}\n\nGive me feedback of my answer to that interview question.`
+    openAIService.getFeedback(prompt)
+      .then((response: any) => feedback.value = response.data.choices[0].message.content)
+      .catch((error: any) => console.log(error))
+  }
 }
 
 watch(currentIndex, () => {
---
+++ src/services/OpenAIService.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export default {
+  getFeedback(content: string) {
+    const apiKey: string | undefined = import.meta.env.VITE_OPENAI_API_KEY;
+
+    return axios.post(
+      "https://api.openai.com/v1/chat/completions",
+      {
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: content }],
+        temperature: 0.7,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${apiKey}`,
+        },
+      }
+    )
+  }
+}
