0c456ecf0532d2c2a4493d11bae55ad92ff62d88
Kevin Lie <kevinyilie@gmail.com>
2023-09-20 20:22:49 -0400

    Add loading message, disable buttons and textarea while feedback is loading

--- src/components/InterviewPrep.vue
+++ src/components/InterviewPrep.vue
@@ -1,10 +1,10 @@
 <template>
   <div v-if="question">
     <p>{{ question }}</p>
-    <textarea v-model="answer"></textarea>
-    <button @click="getFeedback(question, answer)">Submit</button>
+    <textarea v-model="answer" :disabled="isFeedbackLoading"></textarea>
+    <button @click="getFeedback(question, answer)" :disabled="isFeedbackLoading">Submit</button>
     <p>{{ feedback }}</p>
-    <button @click="currentIndex++">Next Question</button>
+    <button @click="currentIndex++" :disabled="isFeedbackLoading">Next Question</button>
   </div>
 
   <div v-else-if="questions.length">
@@ -30,13 +30,19 @@ const question = computed(() => questions[currentIndex.value])
 
 const answer = ref("")
 const feedback = ref("")
+const isFeedbackLoading = ref(false)
 
 function getFeedback(question: string, answer: string) {
   if (question.trim() && answer.trim()) {
     const prompt = `Suppose I'm seeking a role as a junior software developer. I'm being asked this question in an interview: ${question}\n\nThis is my answer: ${answer}\n\nGive me feedback of my answer to that interview question.`
+    
+    isFeedbackLoading.value = true
+    feedback.value = "Generating OpenAI Feedback..."
+    
     openAIService.getFeedback(prompt)
       .then((response: any) => feedback.value = response.data.choices[0].message.content)
-      .catch((error: any) => console.log(error))
+      .catch((error: any) => feedback.value = error.message)
+      .finally(() => isFeedbackLoading.value = false)
   }
 }
 
