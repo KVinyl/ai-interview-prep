270c7be13928a35bef4e0dc9cf8007ee9796b35b
Kevin Lie <kevinyilie@gmail.com>
2023-09-23 20:17:45 -0400

    Add AppHeader styling

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">AI Interview Prep</h1>
+  <div class="bg-white border-gray-400">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <h1 class="text-3xl font-bold text-sky-500">AI Interview Prep</h1>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 
 </script>
-
-<style scoped></style>
