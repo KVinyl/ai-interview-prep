85ed4ae19cfd84fba7f9a1112c37c6009ee1cdcc
Kevin Lie <kevinyilie@gmail.com>
2023-09-26 21:32:46 -0400

    Expand test data

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,10 @@
 </template>
 
 <script setup lang="ts">
+import { interviewQuestionOfTheDayArray } from './assets/data'
+
 import AppHeader from './components/AppHeader.vue'
 import InterviewPreparation from './components/InterviewPreparation.vue'
 
-const questions = ["What is git? Why do we use it?",
-  "What is a variable? Can you give an example of an integer expression? A boolean expression?",
-  "What controls whether the “if” or the “else” portion of an if-else should execute?"
-]
+const questions = interviewQuestionOfTheDayArray
 </script>
-
-<style scoped></style>
