16d77391a863ce729919ec57f0d873c6f85a2385
Kevin Lie <kevinyilie@gmail.com>
2023-10-04 18:25:49 -0400

    Add getDeck to client

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,26 @@
 <template>
-  <div class="bg-sky-100 overflow-auto">
+  <div class="bg-sky-100 overflow-auto min-h-screen">
     <AppHeader />
-    <InterviewPreparation :questions="questions" />
+    <InterviewPreparation v-if="questions.length" :questions="questions" />
+    <div v-else class="text-center">No questions found in the deck</div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { interviewQuestionOfTheDayArray } from './assets/data'
+import { ref, onMounted } from 'vue'
+import type { Ref } from 'vue'
 
 import AppHeader from './components/AppHeader.vue'
 import InterviewPreparation from './components/InterviewPreparation.vue'
 
-const questions = interviewQuestionOfTheDayArray
+import deckService from './services/DeckService'
+
+const questions: Ref<string[]> = ref([])
+const deckId = 1
+
+onMounted(() => {
+  deckService.getDeck(deckId)
+    .then((response) => questions.value = response.data.questions)
+    .catch((error) => console.error(error))
+}) 
 </script>
