c5d13e6e85ab1595932268822b2268c01ba1bdce
Kevin Lie <kevinyilie@gmail.com>
2023-10-04 16:05:04 -0400

    Add Deck and Question models

---
+++ server/AIInterviewPrep/Models/Deck.cs
@@ -0,0 +1,9 @@
+﻿namespace AIInterviewPrep.Models
+{
+    public class Deck
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Question> Questions { get; set; }
+    }
+}
---
+++ server/AIInterviewPrep/Models/Question.cs
@@ -0,0 +1,9 @@
+﻿namespace AIInterviewPrep.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public int DeckId { get; set; }
+    }
+}
