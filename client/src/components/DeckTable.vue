3c63ad8b3167536b92985e64c78ae4e1aae4b044
Kevin Lie <kevinyilie@gmail.com>
2023-10-07 19:04:19 -0400

    Add top rounded corners to DeckTable data if no header

--- client/src/components/DeckTable.vue
+++ client/src/components/DeckTable.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="my-8 drop-shadow-lg">
-        <div v-if="name" class="bg-slate-300 border border-gray-400 border-b-2  p-2 text-l font-semibold rounded-t-lg">{{ name }}</div>
-        <div class="border border-gray-400 divide-y divide-gray-400 overflow-y-auto h-96 rounded-b-lg">
+        <div v-if="name" class="bg-slate-300 border border-gray-400 border-b-2  p-2 text-l font-semibold rounded-t-lg">{{
+            name }}</div>
+        <div class="border border-gray-400 divide-y divide-gray-400 overflow-y-auto h-96 rounded-b-lg"
+            :class="{ 'rounded-t-lg': !name }">
             <DeckTableRow v-for="(question, index) in questions" :number="index + 1" :question="question"
                 :isCurrent="index === currentIndex" @clickText="emit('goToIndex', index)" />
         </div>
