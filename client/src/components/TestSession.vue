2b224bd9f2a92251053e4998a86ab8957ebaae8f
Kevin Lie <kevinyilie@gmail.com>
2023-10-20 00:18:07 -0400

    Add shuffle button functionality

--- client/src/components/TestSession.vue
+++ client/src/components/TestSession.vue
@@ -4,7 +4,7 @@
       <div
         class="flex flex-col items-center space-y-4 px-4 py-4 mt-8 rounded-t-lg bg-gray-200 border border-gray-400 drop-shadow-lg">
         <QuestionSection :question="currentQuestion" />
-        <textarea ref="textarea" v-model="questionsData[index].answer"
+        <textarea ref="textarea" v-model="questionsData[currentIndex].answer"
           class="w-5/6 h-24 rounded-lg border border-gray-400 p-4" placeholder="Enter your answer"
           :disabled="!isUnanswered"></textarea>
         <RectangleButton v-show="isUnanswered" :disabled="isSubmitButtonDisabled" @click="submitAnswer"
@@ -14,7 +14,7 @@
         </RectangleButton>
       </div>
 
-      <NavigationBar :index="index" :status="currentStatus" :isShuffled="isShuffled" @toggleShuffle="toggleShuffle"
+      <NavigationBar :index="currentIndex" :status="currentStatus" :isShuffled="isShuffled" @toggleShuffle="toggleShuffle"
         @previousQuestion="previousQuestion" @nextQuestion="nextQuestion">{{
           currentQuestionNumber }} / {{ questions.length }}</NavigationBar>
 
@@ -25,7 +25,7 @@
       @restartSession="restartSession" />
     <MessageCard v-else message="This deck currently has zero cards." />
 
-    <DeckTable v-if="questionsData.length" :questionsData="questionsData" :name="name" :currentIndex="index"
+    <DeckTable v-if="questionsData.length" :questionsData="questionsData" :name="name" :currentIndex="currentIndex"
       :isDisabled="isGrading" @goToIndex="goToIndex" />
   </div>
 </template>
@@ -49,26 +49,28 @@ const props = defineProps<{
   questions: string[]
 }>()
 
-const index = ref(0)
-const questionsData = ref(props.questions.map((question, index) => ({
+const currentIndex = ref(0)
+const questionsData = ref<QuestionData[]>(props.questions.map((question, index) => ({
   number: index + 1,
   question,
   answer: "",
   feedback: "",
   status: "Unanswered"
-} as QuestionData)))
+})))
 
-const currentQuestionNumber = computed(() => questionsData.value[index.value]?.number)
-const currentQuestion = computed(() => questionsData.value[index.value]?.question)
-const currentAnswer = computed(() => questionsData.value[index.value]?.answer)
-const currentFeedback = computed(() => questionsData.value[index.value]?.feedback)
-const currentStatus = computed(() => questionsData.value[index.value]?.status)
+const currentQuestionData = computed(() => questionsData.value[currentIndex.value])
+
+const currentQuestionNumber = computed(() => currentQuestionData.value.number)
+const currentQuestion = computed(() => currentQuestionData.value.question)
+const currentAnswer = computed(() => currentQuestionData.value.answer)
+const currentFeedback = computed(() => currentQuestionData.value.feedback)
+const currentStatus = computed(() => currentQuestionData.value.status)
 
 const isUnanswered = computed(() => currentStatus.value === "Unanswered")
 const isGrading = computed(() => currentStatus.value === "Grading")
 const isGraded = computed(() => currentStatus.value === "Graded")
 
-const isInSession = computed(() => 0 <= index.value && index.value < questionsData.value.length)
+const isInSession = computed(() => 0 <= currentIndex.value && currentIndex.value < questionsData.value.length)
 const isSubmitButtonDisabled = computed(() => !currentAnswer.value.trim())
 
 const hubUrl = `${import.meta.env.VITE_REMOTE_API}/openAIHub`
@@ -79,7 +81,7 @@ const connection = new HubConnectionBuilder()
 
 const textarea = ref<HTMLInputElement | null>(null)
 
-watch(index, () => {
+watch(currentIndex, () => {
   textarea.value?.focus()
 })
 
@@ -102,31 +104,66 @@ onBeforeUnmount(() => {
 
 connection.on('ReceiveFeedback', (response: string) => {
   if (response !== null) {
-    questionsData.value[index.value].feedback += response
+    questionsData.value[currentIndex.value].feedback += response
   }
 })
 
+function goToIndex(nextIndex: number) {
+  currentIndex.value = nextIndex
+}
+
 const isShuffled = ref(false)
-const shuffledHistory = ref([])
+
+const nextShuffledIndices = computed<number[]>(() =>
+  questionsData.value
+    .map((data, index) => (data.status !== "Graded" ? index : -1))
+    .filter(index => index !== -1 && index !== currentIndex.value)
+)
+
+function getNextShuffledIndex() {
+  if (nextShuffledIndices.value.length > 0) {
+    const randomIndex = Math.floor(Math.random() * nextShuffledIndices.value.length)
+    return nextShuffledIndices.value[randomIndex]
+  }
+  return questionsData.value.length
+}
+
+const prevShuffleHistory: number[] = []
+const nextShuffleHistory: number[] = []
+
+function resetShuffleHistory() {
+  prevShuffleHistory.length = 0
+  nextShuffleHistory.length = 0
+}
 
 function toggleShuffle() {
   isShuffled.value = !isShuffled.value
 }
 
-function resetQuestion() {
-  questionsData.value[index.value].status = "Unanswered"
-}
-
 function previousQuestion() {
-  index.value--
+  if (isShuffled.value) {
+    if (prevShuffleHistory.length > 0) {
+      nextShuffleHistory.unshift(currentIndex.value)
+      const prevIndex = prevShuffleHistory.pop()!
+      goToIndex(prevIndex)
+    }
+  } else {
+    goToIndex(currentIndex.value - 1)
+  }
 }
 
 function nextQuestion() {
-  index.value++
+  if (isShuffled.value) {
+    prevShuffleHistory.push(currentIndex.value)
+    const nextIndex = nextShuffleHistory.length > 0 ? nextShuffleHistory.shift()! : getNextShuffledIndex()
+    goToIndex(nextIndex)
+  } else {
+    goToIndex(currentIndex.value + 1)
+  }
 }
 
-function goToIndex(nextIndex: number) {
-  index.value = nextIndex
+function resetQuestion() {
+  questionsData.value[currentIndex.value].status = "Unanswered"
 }
 
 function restartSession() {
@@ -138,8 +175,8 @@ function goToLastQuestion() {
 }
 
 function submitAnswer() {
-  questionsData.value[index.value].feedback = ""
-  questionsData.value[index.value].status = "Grading"
+  questionsData.value[currentIndex.value].feedback = ""
+  questionsData.value[currentIndex.value].status = "Grading"
 
   const prompt = `Suppose I'm seeking a junior software developer position. 
   I'm being asked this question in an interview: ${currentQuestion.value}
@@ -150,8 +187,8 @@ function submitAnswer() {
     .then(() => console.log('Prompt sent'))
     .catch(error => {
       console.error(error)
-      questionsData.value[index.value].feedback = error
+      questionsData.value[currentIndex.value].feedback = error
     })
-    .finally(() => questionsData.value[index.value].status = "Graded")
+    .finally(() => questionsData.value[currentIndex.value].status = "Graded")
 }
 </script>
