8c45fce5b1d70d93ab5cf861bb303b207a0df70b
Kevin Lie <kevinyilie@gmail.com>
2023-10-22 21:29:11 -0400

    Add DeckTable question filter functionality

--- client/src/components/DeckTable.vue
+++ client/src/components/DeckTable.vue
@@ -4,24 +4,27 @@
       class="flex justify-between bg-slate-300 border border-gray-400 border-b-2 px-4 py-2 text-l font-semibold rounded-t-lg">
       <span class="text-2xl">{{ name }}</span>
       <div class="flex items-center">
-        <input type="text" class="px-3 py-1 rounded-full" placeholder="Filter questions">
+        <input type="text" v-model="filterString" class="px-3 py-1 rounded-full"
+          :class="{ 'ring-2 ring-green-600': filterString }" placeholder="Filter questions">
       </div>
       <MagicAddButton :disabled="isDisabled" @click="emit('clickMagicAdd')" />
     </div>
-    <div class="border border-gray-400 divide-y divide-gray-400 overflow-y-auto h-96 rounded-b-lg"
+    <div class="border border-gray-400 divide-y divide-gray-400 overflow-y-auto max-h-96 rounded-b-lg "
       :class="{ 'rounded-t-lg': !name }">
-      <DeckTableRow v-for="(questionData, index) in questionsData" :key=questionData.number :questionData="questionData"
-        :isCurrent="index === currentIndex" :isDisabled="isDisabled" @clickRowText="emit('jumpToIndex', index)" />
+      <DeckTableRow v-for="questionData in filteredQuestionsData" :key=questionData.number :questionData="questionData"
+        :isCurrent="questionData.number - 1 === currentIndex" :isDisabled="isDisabled"
+        @clickRowText="emit('jumpToIndex', questionData.number - 1)" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref, computed } from 'vue'
 import type { QuestionData } from '../types/QuestionData'
 import DeckTableRow from './DeckTableRow.vue'
 import MagicAddButton from './MagicAddButton.vue'
 
-defineProps<{
+const props = defineProps<{
   questionsData: QuestionData[],
   name?: string,
   currentIndex?: number,
@@ -32,4 +35,7 @@ const emit = defineEmits<{
   jumpToIndex: [nextIndex: number],
   clickMagicAdd: [],
 }>()
+
+const filterString = ref("")
+const filteredQuestionsData = computed(() => props.questionsData.filter(data => data.question.toLowerCase().includes(filterString.value.trim().toLowerCase())))
 </script>
