ff5419e73fdcd9b40ab4675e5801a000ce6a5de4
Kevin Lie <kevinyilie@gmail.com>
2023-10-22 23:09:17 -0400

    Add message when there's zero matches in DataTable filter and conditional filter input ring color

--- client/src/components/DeckTable.vue
+++ client/src/components/DeckTable.vue
@@ -4,16 +4,18 @@
       class="flex justify-between bg-slate-300 border border-gray-400 border-b-2 px-4 py-2 text-l font-semibold rounded-t-lg">
       <span class="text-2xl">{{ name }}</span>
       <div class="flex items-center">
-        <input type="text" v-model="filterString" class="px-3 py-1 rounded-full"
-          :class="{ 'ring-2 ring-green-600': filterString }" placeholder="Filter questions">
+        <input type="text" v-model="filterString" class="px-3 py-1 rounded-full ring-2 focus:ring-2"
+          :class="filterRingColor" placeholder="Filter questions">
       </div>
       <MagicAddButton :disabled="isDisabled" @click="emit('clickMagicAdd')" />
     </div>
     <div class="border border-gray-400 divide-y divide-gray-400 overflow-y-auto max-h-96 rounded-b-lg "
       :class="{ 'rounded-t-lg': !name }">
-      <DeckTableRow v-for="questionData in filteredQuestionsData" :key=questionData.number :questionData="questionData"
-        :isCurrent="questionData.number - 1 === currentIndex" :isDisabled="isDisabled"
-        @clickRowText="emit('jumpToIndex', questionData.number - 1)" />
+      <DeckTableRow v-if="filteredQuestionsData.length > 0" v-for="questionData in filteredQuestionsData"
+        :key=questionData.number :questionData="questionData" :isCurrent="questionData.number - 1 === currentIndex"
+        :isDisabled="isDisabled" @clickRowText="emit('jumpToIndex', questionData.number - 1)" />
+      <div v-else class="px-4 py-2 bg-gray-200 text-red-800 ">Nothing found for <span class="font-bold">{{ filterString
+      }}</span>.</div>
     </div>
   </div>
 </template>
@@ -38,4 +40,16 @@ const emit = defineEmits<{
 
 const filterString = ref("")
 const filteredQuestionsData = computed(() => props.questionsData.filter(data => data.question.toLowerCase().includes(filterString.value.trim().toLowerCase())))
+
+const filterRingColor = computed(() => {
+  if (filterString.value.trim().length === 0) {
+    return ""
+  }
+  else if (filteredQuestionsData.value.length > 0) {
+    return "ring-green-600"
+  }
+  else {
+    return "ring-red-800"
+  }
+})
 </script>
